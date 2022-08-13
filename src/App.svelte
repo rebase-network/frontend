6619d4c78d9b9645c978fef06a8e72b57b22bf6c
liushooter <>
2022-08-14 00:44:36 +0800

    project

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank"> 
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+
+  <h1>Hello, Bootstrap and svelte!</h1>
+  <button class="btn btn-primary">Primary button</button>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
