8b9cb451bdbde10b4aa1114e42c22fe6e9e967d1
dalifornia <shooter.hero@gmail.com>
2022-08-25 10:14:08 +0000

    page

---
+++ src/views/daily.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { onMount } from "svelte";
+  // const url = "http://db.rebase.network/web3daily/public/posts?_order=-id,-time&_page_size=10&_page=1"
+
+  // let posts = [];
+
+  // const opts =  {
+  //   'Content-Type': 'application/json'
+  // }
+</script>
+
+<svelte:head>
+  <title>Rebase Daily view</title>
+</svelte:head>
+
+<main class="container">
+  <div class="container text-center">
+    <div class="row row-cols-4">
+      <div class="col">Column</div>
+      <div class="col">Column</div>
+      <div class="col">Column</div>
+      <div class="col">Column</div>
+    </div>
+  </div>
+</main>
